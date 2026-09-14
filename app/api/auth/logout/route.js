f0cf33cd09ef4a4b1229cd8f3a987f6d408beb4f
hmoreportingschedule-cmyk <hmo.reportingschedule@gmail.com>
2026-09-14 14:02:21 +0530

    Delete app directory

--- app/api/auth/logout/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  const response = NextResponse.json({ ok: true });
-  response.cookies.set("office_user", "", { httpOnly: true, expires: new Date(0), path: "/" });
-  return response;
-}
