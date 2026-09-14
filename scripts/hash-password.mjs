1d8c7ddd05ef3a8c6c96187e623914810f3ef2fd
hmoreportingschedule-cmyk <hmo.reportingschedule@gmail.com>
2026-09-14 14:05:14 +0530

    Delete scripts directory

--- scripts/hash-password.mjs
+++
@@ -1,9 +0,0 @@
-import crypto from "crypto";
-const password = process.argv[2];
-if (!password) {
-  console.log("Usage: npm run hash-password -- YourPassword");
-  process.exit(1);
-}
-const salt = crypto.randomBytes(16);
-const hash = crypto.scryptSync(password, salt, 64);
-console.log(`scrypt$${salt.toString("base64")}$${hash.toString("base64")}`);
